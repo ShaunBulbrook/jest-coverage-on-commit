868068c9ee24e5705ebc31e1d381036c97aacba5
Shaun Bulbrook <sbulbrook@worth.systems>
2021-12-30 09:47:13 +0000

    Testing

--- src/action.js
+++ src/action.js
@@ -1,5 +1,12 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
 async function run() {
-	console.log("hello, world!");
+	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+	const octokit = github.getOctokit(GITHUB_TOKEN);
+	const {context = {}} = github;
+  console.log("🚀 ~ file: action.js ~ line 8 ~ run ~ context", context)
+	console.log("hello, wowrld!");
 }
 
 run();
