83c11c3321d743bf00bb973bd3c3daf8faa037d5
Shaun Bulbrook <sbulbrook@worth.systems>
2021-12-30 10:44:13 +0000

    Add call to write a new commit on the head commit

--- src/action.js
+++ src/action.js
@@ -8,6 +8,23 @@ async function run() {
   console.log("🚀 ~ file: action.js ~ line 8 ~ run ~ context", context)
   console.log("🚀 ~ file: action.js ~ line 8 ~ run ~ context", context.payload.commits)
 	console.log("hello, wowrld!");
+	await writeComment(context.repository.full_name, context.head_commit.id, "hello world")
+}
+
+/**
+ *
+ * @param {string} repoName full name of the repository. ex: "biggles8777/hello-world"
+ * @param {string} commitSha sha for the commit that needs a comment added to it
+ * @param {string} commentContent content for the comment that will be sent
+ */
+const writeComment = async (repoName, commitSha, commentContent) => {
+	const [owner, repo] = repoName.split('/');
+	await octokit.rest.repos.createCommitComment({
+      owner: owner,
+      repo: repo,
+      commit_sha: commitSha,
+      body: inputs.body,
+    });
 }
 
 run();
