a88efa548720f3959ea6bd86b6c903ea63248807
Shaun Bulbrook <sbulbrook@worth.systems>
2021-12-30 11:41:54 +0000

    Run jest tests

--- src/action.js
+++ src/action.js
@@ -1,9 +1,25 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+const github = require('@actions/github')
+const core = require('@actions/core')
+const { exec } = require('@actions/exec')
+const { resolve, join, sep } = require('path')
 
-async function run() {
-	const {context} = github;
-	await writeComment(context.payload.repository.full_name, context.payload.head_commit.id, "hello world")
+async function run () {
+  // Check Token
+  const token = process.env.GITHUB_TOKEN
+  // if (token === undefined) {
+  // 	core.error("GITHUB_TOKEN not set.")
+  // 	core.setFailed("GITHUB_TOKEN not set.")
+  // 	return
+  // }
+
+  // Set working directory
+  const workingDirectory = resolve(core.getInput('working-directory')) + sep
+  const resultsFile = join(workingDirectory, ('jest.results.json'))
+
+  await runJest(getJestCommand(resultsFile), workingDirectory)
+
+  const { context } = github
+  await writeComment(context.payload.repository.full_name, context.payload.head_commit.id, 'hello world')
 }
 
 /**
@@ -13,15 +29,34 @@ async function run() {
  * @param {string} commentContent content for the comment that will be sent
  */
 const writeComment = async (repoName, commitSha, commentContent) => {
-	const [owner, repo] = repoName.split('/');
-	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-	const octokit = github.getOctokit(GITHUB_TOKEN);
-	await octokit.rest.repos.createCommitComment({
-      owner: owner,
-      repo: repo,
-      commit_sha: commitSha,
-      body: commentContent,
-    });
+  const [owner, repo] = repoName.split('/')
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+  const octokit = github.getOctokit(GITHUB_TOKEN)
+  await octokit.rest.repos.createCommitComment({
+    owner: owner,
+    repo: repo,
+    commit_sha: commitSha,
+    body: commentContent
+  })
 }
 
-run();
+// Ripped from https://github.com/mattallty/jest-github-action/blob/master/src/action.ts
+function getJestCommand (resultsFile) {
+  let cmd = core.getInput('test-command', { required: false })
+  const jestOptions = `--testLocationInResults --json --coverage --outputFile=${resultsFile}`
+  const shouldAddHyphen = cmd.startsWith('npm') || cmd.startsWith('npx') || cmd.startsWith('pnpm') || cmd.startsWith('pnpx')
+  cmd += (shouldAddHyphen ? ' -- ' : ' ') + jestOptions
+  core.debug('Final test command: ' + cmd)
+  return cmd
+}
+
+async function runJest (cmd, cwd) {
+  try {
+    await exec(cmd, [], { cwd })
+    console.debug('Jest command executed')
+  } catch (e) {
+    console.error('Tests have likely failed.', e)
+  }
+}
+
+run()
