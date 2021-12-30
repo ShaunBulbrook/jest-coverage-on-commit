f4734555cc61ee390fdf2d89d034052832c1a19e
Shaun Bulbrook <sbulbrook@worth.systems>
2021-12-30 10:49:33 +0000

    Write a hello world comment to the head commit

--- src/action.js
+++ src/action.js
@@ -2,13 +2,8 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 
 async function run() {
-	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-	const octokit = github.getOctokit(GITHUB_TOKEN);
-	const {context = {}} = github;
-  console.log("🚀 ~ file: action.js ~ line 8 ~ run ~ context", context)
-  console.log("🚀 ~ file: action.js ~ line 8 ~ run ~ context", context.payload.commits)
-	console.log("hello, wowrld!");
-	await writeComment(context.repository.full_name, context.head_commit.id, "hello world")
+	const {context} = github;
+	await writeComment(context.payload.repository.full_name, context.payload.head_commit.id, "hello world")
 }
 
 /**
@@ -19,11 +14,13 @@ async function run() {
  */
 const writeComment = async (repoName, commitSha, commentContent) => {
 	const [owner, repo] = repoName.split('/');
+	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+	const octokit = github.getOctokit(GITHUB_TOKEN);
 	await octokit.rest.repos.createCommitComment({
       owner: owner,
       repo: repo,
       commit_sha: commitSha,
-      body: inputs.body,
+      body: commentContent,
     });
 }
 
